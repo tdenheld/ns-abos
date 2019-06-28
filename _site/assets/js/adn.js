f786ee31b43e5ac1836950fad09f5375a08abcc4
Tibor den Held <tibor.denheld@ns.nl>
2019-06-28 15:26:29 +0200

    add factuur page

---
+++ _site/assets/js/adn.js
@@ -0,0 +1,16 @@
+function adn() {
+    const obj = '.js-adn';
+    const $obj = $(obj);
+    const header = obj + '-header';
+    const content = obj + '-content';
+
+    if ($obj[0]) {
+        $obj.each(function () {
+            $(header, this).click(() => {
+                $(this).toggleClass('is-active');
+                $(content, this).slideToggle(200);
+            });
+        });
+    };
+};
+adn();
