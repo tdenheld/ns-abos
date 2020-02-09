ace60ee12ebcdd600847fab871f16f7a9040df91
Tibor den Held <tibordenheld@Apple-of-Tibor.local>
2020-02-09 21:42:06 +0100

    cleanup collapsible.js

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -1,15 +1,13 @@
-function collapsible() {
+(() => {
     const obj = '.js-collapsible';
     const header = obj + '-header';
     const content = obj + '-content';
 
-    if ($(obj)[0]) {
-        $(obj).each(function () {
-            $(header, this).click(() => {
-                $(this).toggleClass('is-active');
-                $(content, this).slideToggle(200);
-            });
+    if (!$(obj)[0]) return
+    $(obj).each(function () {
+        $(header, this).click(() => {
+            $(this).toggleClass('is-active');
+            $(content, this).slideToggle(200);
         });
-    };
-};
-collapsible();
+    });
+})();
